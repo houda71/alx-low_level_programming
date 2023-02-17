11a03e61ed1f6d46d78649b3c25abffaacf58395
Mahmoud <houda.tarek71@gmail.com>
2023-02-17 08:41:48 -0800

    Added 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *   main - Entry point
+ *   Description: prints exactly:
+ *   with proper grammar, but the outcome is a piece of art,
+ *   by using printf
+ *   Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
