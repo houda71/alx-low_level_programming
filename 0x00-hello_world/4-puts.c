6ec301811718be855dc798d80410d579b932b62d
Mahmoud <houda.tarek71@gmail.com>
2023-02-17 07:20:58 -0800

    Added 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *   main - Entry point	
+ *   Description: prints exactly "Programming is like building a multilingual puzzle"
+ *   without using printf
+ *   Return: 0
+ */
+
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+		
+		return (0);
+}
