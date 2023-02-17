418f4784a8ca9ec5e8ed41646da44862b4137604
Mahmoud <houda.tarek71@gmail.com>
2023-02-17 07:27:28 -0800

    Eddited 4-puts.c file: eddited the script to be in Betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
- *   main - Entry point	
- *   Description: prints exactly "Programming is like building a multilingual puzzle"
+ *   main - Entry point
+ *   Description: prints exactly "Programming is like building a
+ *   multilingual puzzle"
  *   without using printf
  *   Return: 0
  */
 
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
+		puts("Programming is like building a multilingual puzzle");
 		
 		return (0);
 }
