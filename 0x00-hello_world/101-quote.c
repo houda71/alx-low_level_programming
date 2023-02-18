2cf69dfe3189170457ccef8b3db3b78b100a4513
Mahmoud <houda.tarek71@gmail.com>
2023-02-17 16:24:49 -0800

    Added 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ *   main - Print a text
+ *   Description: prints exactly and that piece of art is useful
+ *   \" - Dora Korpar, 2015-10-19 by a new line, to the standard error
+ *   Return: 1
+ */
+int main()
+{
+	/* write(file descriptor, message, length); */
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+			, 59);
+
+	return (1);
+}
