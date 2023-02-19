4c4588a147ff0e9d2e36422841e2439d43683dfa
Mahmoud <houda.tarek71@gmail.com>
2023-02-19 12:53:05 -0800

    Eddited 1-last_digit.c: removed trailing whitespaces

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@
 int main(void)
 {
 	int n, last_digit;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
@@ -25,7 +25,7 @@ int main(void)
 	else
 		printf("Last digit of %d is %d and is less than 6 and not 0\n",
 						       n, last_digit);
-	
+
 	return (0);
 }
 
