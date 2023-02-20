4c174e5ed3dbab6d7338852bb49435b384e7981b
Mahmoud <houda.tarek71@gmail.com>
2023-02-20 11:53:43 -0800

    Added 5-print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -  Outout all single digit numbers of base 10 starting from 0,
+ * followed by a new line.
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
