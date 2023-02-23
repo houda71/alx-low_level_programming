f779b0bfb25fd6b30c34b6e38ec20e851fc93d57
Mahmoud <houda.tarek71@gmail.com>
2023-02-23 15:24:47 -0800

    Added 9-times_table.c file. Added the test file 9-main.c Added the prototype of times_table() function to the header file main.h

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+	int x, y, digit1, digit2;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			/* Dont add space before the first column */
+			if (y > 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				/**
+				 * Add another space if printed number
+				 * is single digit.
+				 */
+				if ((x * y) < 10)
+					_putchar(' ');
+			}
+
+			digit2 = (x * y) % 10;
+			digit1 = (x * y) / 10;
+
+			_putchar(digit2 + '0');
+
+			if ((x * y) > 9)
+				_putchar(digit1 + '0');
+		}
+
+		_putchar('\n');
+	}
+}
