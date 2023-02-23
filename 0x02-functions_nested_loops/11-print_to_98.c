8b334078f4e597e1f2f1b3210f62605c0f776336
Mahmoud <houda.tarek71@gmail.com>
2023-02-23 15:59:21 -0800

    Added  11-main.c file. Added the test file 11-main.c Added the prototype of print_to_98() function to the header file main.h

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Pints all natural numbers from n to 98, followed by new line.
+ * @n: the first number and it will passed to the function.
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d", i);
+
+		if (i < 98)
+			printf(", ");
+	}
+
+	printf("\n");
+}
