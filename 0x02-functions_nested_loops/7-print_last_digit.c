7421f8e614c8133bf2c3f3be69ca68a952810f3b
Mahmoud <houda.tarek71@gmail.com>
2023-02-23 07:23:53 -0800

    Added 7-print_last_digit.c file. Added the test file 7-main.c Added the prototype of print_last_digit() function to the header file main.h

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+# include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a passed number.
+ * @num: the integer to check.
+ *
+ * Return: the value of thr last digit.
+ */
+int print_last_digit(int num)
+{
+	int last_digit = num % 10;
+
+	if (num < 0)
+		last_digit = 0 - (num % 10);
+	
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+}
