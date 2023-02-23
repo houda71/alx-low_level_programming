c0b9cf00726762e29a984a8a00aec683ee76510e
Mahmoud <houda.tarek71@gmail.com>
2023-02-23 15:34:27 -0800

    Added 10-add.c file. Added the test file 10-mian.c Added the prototype of add() function to the header file main.h

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result.
+ * @a: the first integer to adding.
+ * @b: the second integer to adding.
+ *
+ * Return: the result of adding.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
