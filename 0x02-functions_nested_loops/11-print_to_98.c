c6cb879b564f9a19bb9fbc197dd44a9f9b08d1cc
Mahmoud <houda.tarek71@gmail.com>
2023-02-23 16:11:54 -0800

    Eddited 11-print_to_98.c to work if the passed number less than 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,12 +9,24 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n > 98)
 	{
-		printf("%d", i);
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i > 98)
+				printf(", ");
+		}
+	}
 
-		if (i < 98)
-			printf(", ");
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i < 98)
+				printf(", ");
+		}
 	}
 
 	printf("\n");
