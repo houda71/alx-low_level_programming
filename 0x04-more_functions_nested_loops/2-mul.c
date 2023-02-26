e8acb56104794b7d45ad8039b3f6cf56fdc6f2e0
Mahmoud <houda.tarek71@gmail.com>
2023-02-26 07:01:31 -0800

    Added 2-mul.c file : multiplay two integers Added the test file 2-main.c Update the header file main.h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers a and b.
+ * @a: first variable
+ * @b: second variable
+ *
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
